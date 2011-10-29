f828ae1423c7bea7dbc4915ec61387cfa96035e9
Flavia Missi <flaviamissi@gmail.com>
2011-10-28 22:28:29 -0200

    first version of setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='kami',
+    version='0.1',
+    description='query solr made easy',
+    author='Flavia Missi',
+    author_email='flaviamissi@gmail.com'
+)
