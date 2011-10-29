5b48ff93444580ab6c82b88432c26e00e70673ef
Flavia Missi <flaviamissi@gmail.com>
2011-10-29 11:16:21 -0200

    Using a class to mount the queries

--- kami/search.py
+++ kami/search.py
@@ -1,8 +1,22 @@
-def search(**params):
-    query = []
-    for key, value in params.items():
-        query.append('AND %s: "%s"' % (key, value))
+class Kami(object):
 
-    query = " ".join(query)[4:]
+    def search(self, **params):
+        return self._mount_query(**params)
 
-    return query
+    def exclude(self, **params):
+        return self._mount_query('NOT ', **params)
+
+    def _mount_query(self, logical_operator='', **params):
+        """
+        Mount the query
+        ``logical_operator`` must end with a blank space, it avoids white spaces
+        when the query doesn't need them
+        ``params`` field=value params
+        """
+        query = []
+        for key, value in params.items():
+            query.append('%s%s: "%s" AND' % (logical_operator, key, value))
+
+        query = " ".join(query)[:-4]
+
+        return query
--- kami/tests/test_search.py
+++ kami/tests/test_search.py
@@ -1,17 +1,32 @@
 from unittest import TestCase
 
-from search import search
+from search import Kami
 
 
 class SimpleSearchQueryTestCase(TestCase):
 
+    def setUp(self):
+        self.kami = Kami()
+
     def test_should_pass_title(self):
-        self.assertEqual('title: "Mein Teil"', search(title='Mein Teil'))
+        self.assertEqual('title: "Mein Teil"', self.kami.search(title='Mein Teil'))
 
     def test_should_pass_artist_and_title(self):
-        query = search(title='Mein Teil', artist='Rammstein')
-        self.assertEqual('artist: "Rammstein" AND title: "Mein Teil"', query)
+        query = self.kami.search(title='Mein Teil', artist='Rammstein')
+        self.assertEqual('title: "Mein Teil" AND artist: "Rammstein"', query)
 
     def test_should_have_one_AND_only_between_a_pair_of_statements(self):
-        query = search(title='Mein Teil', artist='Rammstein', album='Reise, Reise')
-        self.assertEqual('album: "Reise, Reise" AND artist: "Rammstein" AND title: "Mein Teil"', query)
+        query = self.kami.search(title='Mein Teil', artist='Rammstein', album='Reise, Reise')
+        self.assertEqual('album: "Reise, Reise" AND title: "Mein Teil" AND artist: "Rammstein"', query)
+
+
+class SimpleExcludeSearchQueryTestCase(TestCase):
+
+    def setUp(self):
+        self.kami = Kami()
+
+    def test_should_pass_a_title_and_get_its_negation(self):
+        self.assertEqual('NOT title: "Mein Teil"', self.kami.exclude(title='Mein Teil'))
+
+    def test_should_negate_and_concatenate_when_receive_two_fields(self):
+        self.assertEqual('NOT album: "Reise, Reise" AND NOT title: "Amerika"', self.kami.exclude(title='Amerika', album='Reise, Reise'))
